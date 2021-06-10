24b019174e1714d8d91346d076a7dc0c452763f0
GuzovPavel <pguzov.exceedteam@gmail.com>
2021-06-10 16:16:07 +0300

    corrected code

--- src/db/models/task/index.js
+++ src/db/models/task/index.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 
-const {
-  Schema
-} = mongoose;
+const {Schema} = mongoose;
 
 const taskScheme = new Schema({
   text: String,
   date: String,
   sum: Number,
-  // isEditingText: Boolean,
-  // isEditingDate: Boolean,
-  // isEditingSum: Boolean
 })
 
 module.exports = Task = mongoose.model('tasks', taskScheme);
